777b886fee2e7671edfb042b5885be9a633bbee5
SUNIL KUMAR <kumarsunil100403@gmail.com>
2026-09-17 14:09:57 +0530

    Time: 1319 ms (6.53%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,14 @@
+SELECT
+    ROUND(
+        SUM(
+            CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END
+        ) * 100 /
+        COUNT(*),
+        2
+    ) AS immediate_percentage
+FROM Delivery d
+WHERE order_date = (
+    SELECT MIN(order_date)
+    FROM Delivery
+    WHERE customer_id = d.customer_id
+);
